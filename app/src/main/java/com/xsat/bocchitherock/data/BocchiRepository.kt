7cdbeb1020a5e09fa3053411334bb5d92465ae94
satrio <satriocahyo@gmail.com>
2025-02-24 18:35:21 +0700

    Add Favorite Repo.
    Signed-off-by: dontkeep <alsdiaryy@gmail.com>

--- app/src/main/java/com/xsat/bocchitherock/data/BocchiRepository.kt
+++ app/src/main/java/com/xsat/bocchitherock/data/BocchiRepository.kt
@@ -2,12 +2,26 @@ package com.xsat.bocchitherock.data
 
 import com.xsat.bocchitherock.model.Bocchi
 import com.xsat.bocchitherock.model.BocchiData
+import com.xsat.bocchitherock.model.BocchiFavorite
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 
 class BocchiRepository {
+
+    private val bocchiFavorites = mutableListOf<BocchiFavorite>()
+
+    init {
+        if (bocchiFavorites.isEmpty()) {
+            BocchiData.Bocchi.forEach {
+                bocchiFavorites.add(BocchiFavorite(it.id, it.name))
+            }
+        }
+    }
+
     fun getBocchi(): List<Bocchi> {
         return BocchiData.Bocchi
     }
@@ -21,4 +35,31 @@ class BocchiRepository {
     fun getBocchiById(id: String): Flow<Bocchi?> = flow {
         emit(BocchiData.Bocchi.find { it.id == id })
     }.flowOn(Dispatchers.IO)
+
+    fun getFavoriteBocchi(): Flow<List<BocchiFavorite>> {
+        return flowOf(bocchiFavorites)
+    }
+
+    fun updateBocchiFavorite(id: String, name: String): Flow<Boolean>  {
+        val index = bocchiFavorites.indexOfFirst { it.id == id }
+        val result = if (index >= 0) {
+            val bocchiFavorite = bocchiFavorites[index]
+            bocchiFavorites[index] =
+                bocchiFavorite.copy(id = bocchiFavorite.id, name = bocchiFavorite.name)
+            true
+        } else {
+            bocchiFavorites.add(BocchiData.Bocchi.find { it.id == id }?.let { BocchiFavorite(it.id, it.name) }!!)
+            false
+        }
+        return flowOf(result)
+    }
+
+    fun getBocchiFavorite(): Flow<List<BocchiFavorite>> {
+        return getFavoriteBocchi()
+            .map { favorite ->
+                favorite.filter { favorite ->
+                    BocchiData.Bocchi.find { it.id == favorite.id } != null
+                }
+            }
+    }
 }
---
+++ app/src/main/java/com/xsat/bocchitherock/model/BocchiFavorite.kt
@@ -0,0 +1,6 @@
+package com.xsat.bocchitherock.model
+
+data class BocchiFavorite (
+    val id: String,
+    val name: String,
+)
