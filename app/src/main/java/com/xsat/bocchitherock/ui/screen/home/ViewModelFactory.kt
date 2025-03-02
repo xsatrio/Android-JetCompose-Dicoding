313637a0f90af9760c45610335d6660a6b395ac9
xsatrio <satriocahyo93@gmail.com>
2025-03-02 22:29:10 +0700

    Completed FavoriteList.

---
+++ app/src/main/java/com/xsat/bocchitherock/ui/screen/home/ViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.xsat.bocchitherock.ui.screen.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.xsat.bocchitherock.data.BocchiRepository
+import com.xsat.bocchitherock.ui.screen.detail.DetailViewModel
+import com.xsat.bocchitherock.ui.screen.favorite.FavoriteViewModel
+
+@Suppress("UNCHECKED_CAST")
+class ViewModelFactory(private val repository: BocchiRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(HomeViewModel::class.java) -> HomeViewModel(repository)
+            modelClass.isAssignableFrom(DetailViewModel::class.java) -> DetailViewModel(repository)
+            modelClass.isAssignableFrom(FavoriteViewModel::class.java) -> FavoriteViewModel(repository)
+            else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+        } as T
+    }
+
+}
